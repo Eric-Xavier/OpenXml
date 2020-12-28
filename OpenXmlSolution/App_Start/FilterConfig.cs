2823341d0514e63c20ba8af545e7b8e9333eb3de
Eric Xavier <ericxds@hotmail.com>
2020-12-28 14:48:11 -0300

    Inicio do Projeto

---
+++ OpenXmlSolution/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace OpenXmlSolution
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ OpenXmlSolution/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OpenXmlSolution.MvcApplication" Language="C#" %>
